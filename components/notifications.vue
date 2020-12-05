530a232dc7fee826d777bf3cbbb8dd4bfc9b1fd7
ahmadauf98 <ahmadauf98@gmail.com>
2020-12-06 01:07:12 +0800

    code refactor from page to component manager

---
+++ components/notifications.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-snackbar
+      v-show="notification.alert != '' || notification.alert != null"
+      v-model="notification.snackbar"
+      :timeout="notification.timeout"
+      dark
+      top
+    >
+      <div class="d-flex justify-center align-center">
+        <v-icon
+          :class="notification.alertIconStyle"
+          :color="notification.colorIcon"
+          >{{ notification.alertIcon }}</v-icon
+        >
+        {{ notification.alert }}
+      </div>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  // Fetch Notification Data from Vuex
+  computed: { ...mapState(['notification']) },
+}
+</script>
--- pages/manager/auth/browseTournaments/_id/groupseedings.vue
+++ pages/manager/auth/browseTournaments/_id/groupseedings.vue
@@ -3,22 +3,7 @@
     <v-main class="mx-md-5 mx-lg-0 mx-xl-15 px-xl-10 my-0 py-0">
       <v-container class="p-0 my-0" fluid>
         <!-- Notifications -->
-        <v-snackbar
-          v-show="notification.alert != '' || notification.alert != null"
-          v-model="notification.snackbar"
-          :timeout="notification.timeout"
-          dark
-          top
-        >
-          <div class="d-flex justify-center align-center">
-            <v-icon
-              :class="notification.alertIconStyle"
-              :color="notification.colorIcon"
-              >{{ notification.alertIcon }}</v-icon
-            >
-            {{ notification.alert }}
-          </div>
-        </v-snackbar>
+        <notifications />
 
         <!-- Organization Details Part -->
         <tournamentHeader />
@@ -26,234 +11,7 @@
         <v-row>
           <!-- Left Side -->
           <v-col cols="12" lg="8" xl="9" order="2" order-lg="1">
-            <!-- Group Seedings -->
-            <v-card class="mx-auto py-10 mt-n3 mt-lg-0 px-9" outlined tile>
-              <h1 class="text-subtitle-1 font-weight-bold mb-1">
-                Group Seedings
-              </h1>
-
-              <v-row>
-                <!-- Group A -->
-                <v-col
-                  v-show="
-                    tournamentRef.gGroupNumber == 2 ||
-                    tournamentRef.gGroupNumber == 4 ||
-                    tournamentRef.gGroupNumber == 8
-                  "
-                  cols="6"
-                >
-                  <v-card outlined>
-                    <v-simple-table>
-                      <template v-slot:default>
-                        <thead>
-                          <tr>
-                            <th class="text-center">Group A</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr
-                            v-for="(gA, index) in group_A"
-                            :key="index"
-                            class="text-center"
-                          >
-                            <td class="text-center">
-                              {{ gA.teamName }}
-                            </td>
-                          </tr>
-                        </tbody>
-                      </template>
-                    </v-simple-table>
-                  </v-card>
-                </v-col>
-
-                <!-- Group B -->
-                <v-col
-                  v-show="
-                    tournamentRef.gGroupNumber == 2 ||
-                    tournamentRef.gGroupNumber == 4 ||
-                    tournamentRef.gGroupNumber == 8
-                  "
-                  cols="6"
-                >
-                  <v-card outlined>
-                    <v-simple-table>
-                      <template v-slot:default>
-                        <thead>
-                          <tr>
-                            <th class="text-center">Group B</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr
-                            v-for="(gB, index) in group_B"
-                            :key="index"
-                            class="text-center"
-                          >
-                            <td class="text-center">{{ gB.teamName }}</td>
-                          </tr>
-                        </tbody>
-                      </template>
-                    </v-simple-table>
-                  </v-card>
-                </v-col>
-
-                <!-- Group C -->
-                <v-col
-                  v-show="
-                    tournamentRef.gGroupNumber == 4 ||
-                    tournamentRef.gGroupNumber == 8
-                  "
-                  cols="6"
-                >
-                  <v-card outlined>
-                    <v-simple-table>
-                      <template v-slot:default>
-                        <thead>
-                          <tr>
-                            <th class="text-center">Group C</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr
-                            v-for="(gC, index) in group_C"
-                            :key="index"
-                            class="text-center"
-                          >
-                            <td class="text-center">{{ gC.teamName }}</td>
-                          </tr>
-                        </tbody>
-                      </template>
-                    </v-simple-table>
-                  </v-card>
-                </v-col>
-
-                <!-- Group D -->
-                <v-col
-                  v-show="
-                    tournamentRef.gGroupNumber == 4 ||
-                    tournamentRef.gGroupNumber == 8
-                  "
-                  cols="6"
-                >
-                  <v-card outlined>
-                    <v-simple-table>
-                      <template v-slot:default>
-                        <thead>
-                          <tr>
-                            <th class="text-center">Group D</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr
-                            v-for="(gD, index) in group_D"
-                            :key="index"
-                            class="text-center"
-                          >
-                            <td class="text-center">{{ gD.teamName }}</td>
-                          </tr>
-                        </tbody>
-                      </template>
-                    </v-simple-table>
-                  </v-card>
-                </v-col>
-
-                <!-- Group E -->
-                <v-col v-show="tournamentRef.gGroupNumber == 8" cols="6">
-                  <v-card outlined>
-                    <v-simple-table>
-                      <template v-slot:default>
-                        <thead>
-                          <tr>
-                            <th class="text-center">Group E</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr
-                            v-for="(gE, index) in group_E"
-                            :key="index"
-                            class="text-center"
-                          >
-                            <td class="text-center">{{ gE.teamName }}</td>
-                          </tr>
-                        </tbody>
-                      </template>
-                    </v-simple-table>
-                  </v-card>
-                </v-col>
-
-                <!-- Group F -->
-                <v-col v-show="tournamentRef.gGroupNumber == 8" cols="6">
-                  <v-card outlined>
-                    <v-simple-table>
-                      <template v-slot:default>
-                        <thead>
-                          <tr>
-                            <th class="text-center">Group F</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr
-                            v-for="(gF, index) in group_F"
-                            :key="index"
-                            class="text-center"
-                          >
-                            <td class="text-center">{{ gF.teamName }}</td>
-                          </tr>
-                        </tbody>
-                      </template>
-                    </v-simple-table>
-                  </v-card>
-                </v-col>
-
-                <!-- Group G -->
-                <v-col v-show="tournamentRef.gGroupNumber == 8" cols="6">
-                  <v-card outlined>
-                    <v-simple-table>
-                      <template v-slot:default>
-                        <thead>
-                          <tr>
-                            <th class="text-center">Group G</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr
-                            v-for="(gG, index) in group_G"
-                            :key="index"
-                            class="text-center"
-                          >
-                            <td class="text-center">{{ gG.teamName }}</td>
-                          </tr>
-                        </tbody>
-                      </template>
-                    </v-simple-table>
-                  </v-card>
-                </v-col>
-
-                <!-- Group H -->
-                <v-col v-show="tournamentRef.gGroupNumber == 8" cols="6">
-                  <v-card outlined>
-                    <v-simple-table>
-                      <template v-slot:default>
-                        <thead>
-                          <tr>
-                            <th class="text-center">Group H</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr
-                            v-for="(gH, index) in group_H"
-                            :key="index"
-                            class="text-center"
-                          >
-                            <td class="text-center">{{ gH.teamName }}</td>
-                          </tr>
-                        </tbody>
-                      </template>
-                    </v-simple-table>
-                  </v-card>
-                </v-col>
-              </v-row>
-            </v-card>
+            <groupseedingscomp />
           </v-col>
 
           <!-- Right Side -->
@@ -267,9 +25,10 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
 import tournamentHeader from '~/components/tournamentHeader'
 import tournamentInfo from '~/components/manager/tournamentInfo'
+import groupseedingscomp from '~/components/manager/groupseedingscomp'
+import notifications from '~/components/notifications'
 
 export default {
   middleware: 'authenticated',
@@ -279,62 +38,8 @@ export default {
   components: {
     tournamentHeader,
     tournamentInfo,
+    groupseedingscomp,
+    notifications,
   },
-
-  data() {
-    return {
-      // User Input Data
-      tournamentRef: '',
-
-      // Team Draw Data
-      group_A: '',
-      group_B: '',
-      group_C: '',
-      group_D: '',
-      group_E: '',
-      group_F: '',
-      group_G: '',
-      group_H: '',
-    }
-  },
-
-  // Fetch Notification Data from Vuex
-  computed: { ...mapState(['notification']) },
-
-  // Fetch Data from Firestore
-  mounted() {
-    // Tournament Data
-    this.$fire.firestore
-      .collection('tournaments')
-      .doc(this.$route.params.id)
-      .onSnapshot((doc) => {
-        this.tournamentRef = doc.data()
-      })
-
-    // Get Bracket Group
-    this.$fire.firestore
-      .collection('tournaments')
-      .doc(this.$route.params.id)
-      .collection('group-stage')
-      .doc('seedings')
-      .onSnapshot((doc) => {
-        if (doc.exists) {
-          // Read Data
-          this.group_A = doc.data().group_A
-          this.group_B = doc.data().group_B
-          this.group_C = doc.data().group_C
-          this.group_D = doc.data().group_D
-          this.group_E = doc.data().group_E
-          this.group_F = doc.data().group_F
-          this.group_G = doc.data().group_G
-          this.group_H = doc.data().group_H
-
-          // Overlay Data
-          this.groupStageData = doc.data()
-        }
-      })
-  },
-
-  methods: {},
 }
 </script>
