f9113da66bdd18141d39c49ebb02b8ee05a3508c
Ahmad Auf Bin Nasruddin <ahmadauf98@gmail.com>
2020-12-23 20:05:40 +0800

    Initial add component for manager - events

---
+++ pages/manager/auth/events/_id/_tournamentID/finalstage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Final Stage</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/manager/auth/events/_id/_tournamentID/standings.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Standings</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
